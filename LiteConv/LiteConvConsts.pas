bd0e96892f637e670e1d34727d6639cd51d7ee86
Freeman <vapaamies@yandex.ru>
2013-08-27 03:58:40 +0400

    UniConv → LiteConv, restored as a possible CoreLite test

---
+++ LiteConv/LiteConvConsts.pas
@@ -0,0 +1,55 @@
+(*
+    LiteConv string constants and messages
+
+    Copyright © 2013 Vladislav Javadov (Freeman)
+*)
+
+unit LiteConvConsts;
+
+interface
+
+{ Do not localize }
+
+const
+  CP_CORE = 1252;
+
+  sInto     = 'into';
+  sCodePage = 'cp';
+  sCharSet  = 'cs';
+  sPause    = 'pause';
+
+{ Localize }
+
+const
+  sUsage  = 'Usage:'#9'%ws source-file [<charset>] [-into dest-file [<charset>]] [-pause]'#10#10 +
+            'Common options'#10 +
+                    #9'-into dest-file'#9'Write result to the dest-file'#10 +
+                    #9'-pause'#9#9'Wait for ENTER key to exit'#10#10 +
+            'Character set options for <charset>'#10 +
+                    #9'-cp N'#9'Use code page N to decode/encode file'#10 +
+                  //  #9'-cs S'#9'Use character set S to decode/encode file'#10 +
+                    #9'-oem'#9'Use OEM code page to decode/encode file'#10 +
+                    #9'-8'#9'Encode to CESU-8'#10 +
+                    #9'-16'#9'Encode to UTF-16'#10 +
+                    #9'-32'#9'Encode to UTF-32'#10 +
+                    #9'-BE'#9'Encode UTF-16 or UTF-32 as big-endian'#10#10 +
+            'Current environment'#10 +
+                    #9'ANSI code page (ACP)'#9'%u'#10+
+                    #9'OEM code page (OEMCP)'#9'%u';
+
+  sEnvCodePage = '%u (%u bytes per character)';
+
+  sWarning = 'Warning';
+  sNothingToSave = 'Nothing to save';
+
+  sInvalidCodePage = 'Invalid code page number “%s”';
+  sMissingCodePage = 'Missing code page number';
+
+  sPressEnterToExit = 'Press ENTER to exit';
+
+implementation
+
+end.
+
+
+
---
+++ LiteConv/LiteConvCore.pas
@@ -0,0 +1,282 @@
+(*
+    LiteConv core engine
+
+    Copyright © 2013 Vladislav Javadov (Freeman)
+*)
+
+unit LiteConvCore;
+
+interface
+
+uses
+  CoreUtils, CoreWrappers, CoreClasses, CoreStrings, CoreExceptions;
+
+type
+  TCodePageArg = (cpAuto, cpOEM, cpCESU8, cpUTF16LE, cpUTF16BE, cpUTF32LE, cpUTF32BE);
+
+  TFileArg = class(TListItem)
+  private
+  { hold } FPrior, FNext: TFileArg;
+    FFileName: TCoreString;
+    FCodePage: Word;
+  public
+    constructor Create(FileName: PCoreChar; Count: Integer);
+    destructor Destroy; override; 
+
+    property CodePage: Word read FCodePage write FCodePage;
+    property FileName: TCoreString read FFileName;
+    property Next: TFileArg read FNext;
+    property Prior: TFileArg read FPrior;
+  end;
+
+  TFileArgs = class(TList)
+  private
+  { hold } FFirst, FLast: TFileArg;
+  public
+    property First: TFileArg read FFirst;
+    property Last: TFileArg read FLast;
+  end;
+
+  TApplication = class
+  private
+    FConsole: TStreamConsole;
+    FAppName: PCoreChar;
+    FFileArgs: TFileArgs;
+    FInto: TFileArg;
+    FPause: Boolean;
+    procedure Warning(Msg: PLegacyChar); overload;
+    procedure Warning(Msg: PLegacyChar; const Args: array of const); overload;
+  public
+    constructor Create(CommandLine: PCoreChar);
+    destructor Destroy; override;
+    procedure Run;
+  end;
+
+  {TProcessor = class
+  private
+  public
+    constructor Create(const FileNames: TInputFileNames);
+    destructor Destroy; override;
+    function Execute(Into: TLegacyStrings; App: TApplication;
+      var FuncCount: Integer): Integer;
+  end;}
+
+  ECommandLine = class(Exception)
+  private
+    FInvalidArg: PCoreChar;
+  public
+    constructor Create(Msg: PLegacyChar; Arg: PCoreChar);
+    property InvalidArg: PCoreChar read FInvalidArg;
+  end;
+
+  ECore = class(Exception);
+
+implementation
+
+uses
+  Windows, LiteConvConsts;
+
+const
+  CSTR_EQUAL = 2;
+
+type
+  TLineBuf = array[0..80] of LegacyChar; // including #0
+
+{ ECommandLine }
+
+constructor ECommandLine.Create(Msg: PLegacyChar; Arg: PCoreChar);
+begin
+  inherited Create(Msg, CP_CORE, [Arg]);
+end;
+
+{ TFileArg }
+
+constructor TFileArg.Create(FileName: PCoreChar; Count: Integer);
+begin
+  FFileName := TCoreString.Create;
+  FFileName.Insert(FileName, Count, [soAttachBuffer]); // soAttachBuffer unsafe for other environment
+end;
+
+destructor TFileArg.Destroy;
+begin
+  FFileName.Free;
+  inherited;
+end;
+
+{ TApplication }
+
+constructor TApplication.Create(CommandLine: PCoreChar);
+
+function SameKey(const P: TWideParamRec; Key: PWideChar): Boolean;
+begin
+  Result := CompareStringW(LOCALE_USER_DEFAULT, NORM_IGNORECASE, Key, WideStrLen(Key),
+    P.Param + 1, P.Length - 1) = CSTR_EQUAL;
+end;
+
+function ToCodePage(const P: TWideParamRec): Word;
+var
+  Limit, W: PCoreChar;
+begin
+  Result := 0;
+  with P do
+  begin
+    Limit := Param + Length;
+    W := Param;
+  end;
+  while (W < Limit) and (W^ >= CoreChar('0')) and (W^ <= CoreChar('9')) do
+  begin
+    Result := Result * 10 + Word(W^) - Word('0');
+    Inc(W);
+  end;
+  if W <> Limit then
+  begin
+    Limit^ := CoreChar(0); // unsafe;
+    raise ECommandLine.Create(sInvalidCodePage, P.Param);
+  end;
+end;
+
+var
+  Dot: PCoreChar;
+  P: TWideParamRec;
+  ExeName: array[0..MAX_PATH] of CoreChar;
+  Arg: TFileArg;
+  CP: Word;
+  Into: Boolean;
+begin
+  FConsole := TStreamConsole.Create;
+  with FConsole do
+  begin
+    CodePage := GetACP;
+    WriteLn;
+  end;
+
+  if GetModuleFileNameW(0, ExeName, Length(ExeName)) = 0 then
+    RaiseLastPlatformError;
+  with TVersionInfo.Create(ExeName) do
+  try
+    if TranslationCount <> 0 then
+      FConsole.WriteLn('%ws %ws  %ws', [StringInfo(0, 'ProductName'),
+        StringInfo(0, 'ProductVersion'), StringInfo(0, 'LegalCopyright')], 2);
+  finally
+    Free;
+  end;
+
+  with WideParamStr(CommandLine) do
+  begin
+    FAppName := WideStrRScan(Param, PathDelimiter, Length);
+    if FAppName <> nil then
+      Inc(FAppName)
+    else
+      FAppName := Param;
+
+    Dot := WideStrScan(FAppName, '.', Length - (PLegacyChar(FAppName) - PLegacyChar(Param)) div SizeOf(CoreChar));
+    if Dot <> nil then
+      Dot^ := CoreChar(0) // unsafe
+    else
+      Param[Length] := CoreChar(0); // unsafe
+
+    CommandLine := NextParam;
+  end;
+
+  FFileArgs := TFileArgs.Create;
+
+  repeat
+    P := WideParamStr(CommandLine);
+    if P.Length = 0 then
+      Break;
+
+    if (not P.Quoted) and ((P.Param^ = CoreChar('/')) or (P.Param^ = CoreChar('-'))) then
+    begin
+      if SameKey(P, sCodePage) then
+      begin
+        P := WideParamStr(P.NextParam);
+        if P.Length = 0 then
+          raise ECommandLine.Create(sMissingCodePage, nil);
+        if FInto <> nil then
+          FInto.CodePage := ToCodePage(P)
+        else
+        begin
+          Arg := FFileArgs.Last;
+          CP := ToCodePage(P);
+          while Arg <> nil do
+          begin
+            Arg.CodePage := CP;
+            Arg := Arg.Prior;
+          end;
+        end;
+        P.Param := nil;
+      end
+      else if SameKey(P, sInto) then
+      begin
+        Into := True;
+        P.Param := nil;
+      end
+      else if SameKey(P, sPause) then
+      begin
+        FPause := True;
+        P.Param := nil;
+      end;
+    end;
+
+    if P.Param <> nil then
+    begin
+    
+    end;
+
+    CommandLine := P.NextParam;
+  until False;
+end;
+
+destructor TApplication.Destroy;
+begin
+  if FPause then // placed here to show exceptions properly
+    FConsole.ReadLn(sPressEnterToExit);
+  FInto.Free;
+  FFileArgs.Free;
+  FConsole.Free;
+end;
+
+procedure TApplication.Run;
+begin
+  if FFileArgs.First = nil then
+  begin
+    FConsole.WriteLn(sUsage, [FAppName, GetACP, GetOEMCP]);
+    Exit;
+  end;
+end;
+
+procedure TApplication.Warning(Msg: PLegacyChar);
+begin
+  FConsole.WriteLn(sWarning, [Msg]);
+end;
+
+procedure TApplication.Warning(Msg: PLegacyChar; const Args: array of const);
+var
+  Buf: TLineBuf;
+begin
+  FormatBuf(Msg, Args, Buf);
+  Warning(Buf);
+end;
+
+{ TProcessor }
+
+{constructor TProcessor.Create(const FileNames: TInputFileNames);
+begin
+end;
+
+destructor TProcessor.Destroy;
+begin
+//  inherited;
+end;
+
+function TProcessor.Execute(Into: TLegacyStrings; App: TApplication;
+  var FuncCount: Integer): Integer;
+
+var
+  Limit: PLegacyChar;
+
+begin
+end;}
+
+end.
+
