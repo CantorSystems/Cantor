26ae4df32f85477c3b5316da6cf46a4c2cc4a757
Freeman <vapaamies@yandex.ru>
2016-07-02 14:48:12 +0000

    Pet command line keys converted to lower case

--- Tools/Pet/PetConsts.pas
+++ Tools/Pet/PetConsts.pas
@@ -18,8 +18,8 @@ const         // short Unicode strings
   sDump       = #4'dump';
   sVersion    = #7'version';
 
-  s3GB        = #3'3GB';
-  sASLR       = #4'ASLR';
+  s3GB        = #3'3gb';
+  sASLR       = #4'aslr';
   sAuto       = #4'auto';
   sDir        = #3'dir';
   sDeep       = #4'deep';
@@ -28,7 +28,7 @@ const         // short Unicode strings
   sLog        = #3'log';
   sMenuet     = #7'menuet';
   sNoLogo     = #6'nologo';
-  sNX         = #2'NX';
+  sNX         = #2'nx';
   sPause      = #5'pause';
   sRaw        = #3'raw';
   sRebase     = #6'rebase';
@@ -63,9 +63,9 @@ const
     'Option for -extract and -into commands'#10 +
             #9'-strip' +         #9#9#9'Strip headers, exports, debug information,'#10 +
                                #9#9#9#9'relocations and empty sections when possible'#10#10 +
-    'Options for -into command'#10 +
-            #9'-3GB' +           #9#9#9'Set large address aware flag'#10 +
-            #9'-ASLR' +          #9#9#9'Set dynamic image base flag'#10 +
+    'Options for estimation and -into command'#10 +
+            #9'-3gb' +           #9#9#9'Set large address aware flag'#10 +
+            #9'-aslr' +          #9#9#9'Set dynamic image base flag (ASLR)'#10 +
             {#9'-auto [#[%]]|[.]'    +#9'Using '#$E2#$80#$9C'-keep -strip -trunc -stub'#$E2#$80#$9D' mode, replace'#10 +
                                #9#9#9#9'original file when stripped size is equal or'#10 +
                                #9#9#9#9'greater given number of bytes, percent or'#10 +
@@ -75,7 +75,7 @@ const
             #9'-dropsect <s1,s2,...>'#9'Drop sections <s1>, <s2>, etc.'#10 +
             #9'-log <actions|totals>'#9'Output log style'#10 +
             #9'-menuet' +        #9#9#9'Save to MENUET01/MENUET02 format (alpha stage!)'#10 +
-            #9'-NX' +            #9#9#9'Set DEP compatibility flag'#10 +
+            #9'-nx' +            #9#9#9'Set DEP compatibility flag (NX)'#10 +
             #9'-osver <#[.#]>' +   #9#9'Set required OS version to <#> or <#.#>'#10 +
             #9'-raw' +           #9#9#9'Don''t align raw data size values at section'#10 +
                                #9#9#9#9'headers (to avoid antivirus false positives)'#10 +
