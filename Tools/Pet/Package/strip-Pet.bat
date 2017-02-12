9ac405453b70f05da6e0d2ce83ba74438e0256cc
Freeman <vapaamies@yandex.ru>
2017-02-12 22:25:45 +0000

    Batch-style output in strip-Pet.bat

--- Tools/Pet/Package/strip-Pet.bat
+++ Tools/Pet/Package/strip-Pet.bat
@@ -1,3 +1,3 @@
 @echo off
-call "%~dp0strip" -raw "%~dp0Pet.exe" -into "%Temp%\Pet.exe"
+call "%~dp0strip" -nologo -log totals -raw "%~dp0Pet.exe" -into "%Temp%\Pet.exe"
 move /y "%Temp%\Pet.exe" "%~dp0Pet.exe"
